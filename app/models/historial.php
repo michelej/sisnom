e60b75610bf14e93dc88ee11d44830bce9a114ea
Michel Escobar <michelejcs@hotmail.com>
2012-03-14 11:49:13 -0400

    Arreglos y comentarios en historial y la funcion de verificar los rangos de fechas

--- app/models/historial.php
+++ app/models/historial.php
@@ -15,70 +15,9 @@ class Historial extends AppModel {
      *
      * @return boolean 
      */
-    function beforeSave() {
-        $this->recursive = -1;
-        $historiales = $this->findAllByCargoId($this->data['Historial']['cargo_id']);
-
-        $fecha_ini = $this->data['Historial']['FECHA_INI'];
-        $fecha_fin = $this->data['Historial']['FECHA_FIN'];
-
-        $hoy = date("d-m-Y");
-        // Si fecha final es null quiere decir que estamos tratando con rango de sueldos actual
-        if ($fecha_fin == null) {
-            $fecha_fin = $hoy;
-            $actual = TRUE;
-            $this->data['Historial']['FECHA_FIN'] = NULL;
-        } else {
-            $actual = FALSE;
-        }
-
-        // Verificar que fecha final sea mayor a fecha inicial
-        if ($fecha_ini > $fecha_fin) {
+    function beforeSave() {        
+        if (!$this->validacionFechas()) {
             return false;
-        }
-        // No se permiten fechas futuras
-        if ($fecha_ini > $hoy) {
-            return false;
-        }
-        // Si existe algun historial se hacen varios chequeos 
-        if (!empty($historiales)) {
-            foreach ($historiales as $historial) {
-                $fecha_i = $historial['Historial']['FECHA_INI'];
-                $fecha_f = $historial['Historial']['FECHA_FIN'];
-
-                if ($fecha_f == NULL) {
-                    $fecha_f = $hoy;
-                    // si es un nuevo rango actual , osea un rango con fecha_fin = null
-                    if ($actual) {
-                        if (!check_in_range($fecha_i, $fecha_f, $fecha_ini)) {
-                            return false;
-                        } else {
-                            $fecha = formatoFechaBeforeSave($fecha_ini);
-                            // buscamos la fecha inicial y le restamos 1 day para formar la nueva fecha final
-                            // del historial anterior
-                            $fecha_new = date("Y-m-d", strtotime($fecha . "-1 days"));
-                            $id = $historial['Historial']['id'];
-                            $this->query("update historiales set FECHA_FIN='" . $fecha_new . "' where id='" . $id . "'");
-                        }
-                    } else {
-                        if ($fecha_ini>$fecha_i && $fecha_ini>$fecha_f) {
-                            $fecha = formatoFechaBeforeSave($fecha_ini);
-                            // buscamos la fecha inicial y le restamos 1 day para formar la nueva fecha final
-                            // del historial anterior
-                            $fecha_new = date("Y-m-d", strtotime($fecha . "-1 days"));
-                            $id = $historial['Historial']['id'];
-                            $this->query("update historiales set FECHA_FIN='" . $fecha_new . "' where id='" . $id . "'");
-                        }
-                    }
-                } else {
-                    if (check_in_range($fecha_i, $fecha_f, $fecha_ini)) {
-                        return false;
-                    }
-                    if (check_in_range($fecha_i, $fecha_f, $fecha_fin)) {
-                        return false;
-                    }
-                }
-            }
         }
 
         //Tratamos las fechas
@@ -107,6 +46,90 @@ class Historial extends AppModel {
             }
         }
         return $results;
+    }
+
+    function validacionFechas() {
+        // Ojo las fechas que terminan en NULL se consideran como PRESENTE solo existe una ,
+        // ademas no se permite crear rangos futuros a la fecha obtenida con date()
+        // 
+        
+        // Buscamos todos los Historiales de este cargo
+        $this->recursive = -1;
+        $historiales = $this->findAllByCargoId($this->data['Historial']['cargo_id']);
+
+        $fecha_ini = $this->data['Historial']['FECHA_INI'];
+        $fecha_fin = $this->data['Historial']['FECHA_FIN'];
+
+        $hoy = date("d-m-Y");
+        // Si la nueva fecha final es null quiere decir que estamos tratando con rango de sueldos actual
+        if ($fecha_fin == null) {
+            $fecha_fin = $hoy;  // declaramos la fecha final como la de hoy
+            $actual = TRUE;    // estamos trabajando con un nuevo rango actual
+            $this->data['Historial']['FECHA_FIN'] = NULL;  // null para la base de datos
+        } else {
+            $actual = FALSE;  // es un rango de fechas normal
+        }
+
+        // Verificar que fecha final sea mayor a fecha inicial , valides del rango
+        if ($fecha_ini > $fecha_fin) {
+            return false;
+        }
+        // No se permiten fechas futuras
+        if ($fecha_ini > $hoy) {
+            return false;
+        }
+        // Si existe algun historial se hacen varios chequeos 
+        if (!empty($historiales)) {
+            foreach ($historiales as $historial) {
+                $fecha_i = $historial['Historial']['FECHA_INI'];
+                $fecha_f = $historial['Historial']['FECHA_FIN'];
+
+                //si la fecha del historial es null estamos tratando con un rango actual                
+                // idealmente solo existe uno y es el ultimo
+                if ($fecha_f == NULL) {
+                    $fecha_f = $hoy;
+                    // si quiero crear un nuevo rango actual y ya tengo uno en historial
+                    if ($actual) {
+                        // la nueva fecha actual debe estar dentro del rango de la fecha actual que existe
+                        // en el historial de lo contrario no nos sirve
+                        if (check_in_range($fecha_i, $fecha_f, $fecha_ini)) {
+                            $fecha = formatoFechaBeforeSave($fecha_ini);
+                            // buscamos la fecha inicial y le restamos 1 day para formar la nueva fecha final
+                            // del historial anterior
+                            $fecha_new = date("Y-m-d", strtotime($fecha . "-1 days"));
+                            $id = $historial['Historial']['id'];
+                            $this->query("update historiales set FECHA_FIN='" . $fecha_new . "' where id='" . $id . "'");
+                            return true; // listo OJO idealmente el ultimo rango es el que tiene NULL en su fecha final
+                        } else {
+                            return false;
+                        }
+                     // si no estoy creando un rango actual , osea un rango normal
+                    } else {
+                        if ($fecha_ini > $fecha_i) {
+                            $fecha = formatoFechaBeforeSave($fecha_ini);
+                            // buscamos la fecha inicial y le restamos 1 day para formar la nueva fecha final
+                            // del historial anterior
+                            $fecha_new = date("Y-m-d", strtotime($fecha . "-1 days"));
+                            $id = $historial['Historial']['id'];
+                            $this->query("update historiales set FECHA_FIN='" . $fecha_new . "' where id='" . $id . "'");
+                            return true;
+                        }
+                    }
+                } else {
+                    // simplemente no se pueden solapar los rangos
+                    if (check_in_range($fecha_i, $fecha_f, $fecha_ini) || check_in_range($fecha_i, $fecha_f, $fecha_fin)) {
+                        return false;
+                    }
+                }
+
+                // el nuevo rango de fechas no puede contener a ninguno de los rangos que ya existen
+                if (check_in_range($fecha_ini, $fecha_fin, $fecha_i) || check_in_range($fecha_ini, $fecha_fin, $fecha_f)) {
+                    return false;
+                }
+            }
+        }
+        // si pasamos todos las verificaciones damos la luz verde
+        return true;
     }
 
 }
