0048918c1756bdca2ac948f7d9ccf5c7378bd0d6
Michel Escobar <michelejcs@hotmail.com>
2012-05-07 17:52:20 -0400

    Implementacion simple de autentificacion

--- app/app_controller.php
+++ app/app_controller.php
@@ -8,15 +8,56 @@ class AppController extends Controller {
         ),
         'Session'
     );
-    
-    function beforeFilter(){
-        $login = $this->Authsome->get();                
-        if(empty($login) && $this->action!='login'){  
-            $controller=$this->name;
-            $action=$this->action;
-            $this->Session->write('loginRedirect','/'.$this->params['url']['url']);
+
+    function beforeFilter() {
+        $login = $this->Authsome->get();
+        $controller = $this->name;
+        $action = $this->action;
+        if (empty($login) && $this->action != 'login') {
+            $this->Session->write('loginRedirect', '/' . $this->params['url']['url']);
             $this->redirect('/users/login');
-        }                
+        }
+
+        if (!empty($login)) {
+            if (!$this->tienePermiso($controller, $action)) {
+                $this->redirect('/permiso_negado');
+            }
+        }
+    }
+
+    function tienePermiso($controller, $action) {
+        $permisos = array(
+            'Todos'=>array(
+              'Pages','Users'  
+            ),
+            'Usuario' => array(
+                'Empleados'
+            ),
+            'Administrador' => array(
+                '*'
+            )
+        );
+
+        //debug($controller);
+        //debug($action);
+        $grupo = Authsome::get('GRUPO');        
+        $flag=false;
+        foreach ($permisos[$grupo] as $value) {
+            if($value=='*'){
+                $flag=true;
+            }elseif ($value==$controller) {
+                $flag=true;
+            }
+        }
+        foreach ($permisos['Todos'] as $value) {
+            if($value=='*'){
+                $flag=true;
+            }elseif ($value==$controller) {
+                $flag=true;
+            }
+        }
+        
+        return $flag;
     }
 
 }
---
+++ app/views/pages/permiso_negado.ctp
@@ -0,0 +1,9 @@
+<div class="box">  
+    <div class="title"></div>
+    <div class="content form">
+
+        <div class="message red">
+            Usted no tiene acceso a este recurso.
+        </div> 
+    </div>
+</div>
