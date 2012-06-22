9ff44e7c407f957cc8750ffe2dfb45d45f15b19f
Michel Escobar <michelejcs@hotmail.com>
2012-06-22 00:13:13 -0430

    Reportes

--- app/controllers/reportes_controller.php
+++ app/controllers/reportes_controller.php
@@ -17,68 +17,70 @@ class ReportesController extends AppController {
                 $modalidad = "Fijo";
             }
             if ($this->data['TIPO'] == '3') {
-                $grupo = array("Empleado", "Obrero");
+                $grupo = array("Empleado", "Obrero");                
                 $modalidad = "Contratado";
             }
 
-            $ids = $this->Empleado->Grupo->find('all', array(
-                'conditions' => array(
-                    'NOMBRE' => $grupo
-                ),
-                'contain' => array(
-                    'Empleado' => array(
-                        'fields' => array(
-                            'id'
-                        )
-                    )
-                )
-                    ));
-            $id_empleados = Set::extract('/Empleado/id', $ids);
-
-            $hoy = date("d-m-Y");
-            $ids = $this->Contrato->find('all', array(
-                'conditions' => array(
-                    'OR' => array(
-                        'FECHA_FIN > ' => $hoy,
-                        'FECHA_FIN' => NULL,
-                    ),
-                    'AND' => array(
-                        'MODALIDAD' => $modalidad,
-                        'empleado_id' => $id_empleados,
-                    )
-                ),
-                'contain' => array(
-                    'Empleado' => array(
-                        'fields' => array(
-                            'id')
-                    )
-                )
-                    ));
-
-            $id_empleados = Set::extract('/Empleado/id', $ids);
-
-            $this->paginate = array(
-                'Empleado' => array(
-                    'limit' => 20,
-                    'conditions' => array(
-                        'Empleado.id' => $id_empleados,
-                    ),
-                    'contain' => array(
-                        'Contrato' => array(
-                            'order' => array('Contrato.FECHA_INI' => 'asc'),
-                            'Cargo', 'Departamento'
-                        )
-                    )
-                )
-            );
-
             if ($this->data['MODO'] == "1") {
-                $data = $this->paginate('Empleado');
-                $this->set('empleados', $data);
-                $this->render('pantalla_listado');
+                if(is_array($grupo)){
+                    $grupo="Todos";
+                }
+                $this->redirect('pantalla_listado/' . $grupo . "/" . $modalidad);
                 return;
             }
             if ($this->data['MODO'] == "2") {
+                
+                $ids = $this->Empleado->Grupo->find('all', array(
+                    'conditions' => array(
+                        'NOMBRE' => $grupo
+                    ),
+                    'contain' => array(
+                        'Empleado' => array(
+                            'fields' => array(
+                                'id'
+                            )
+                        )
+                    )
+                        ));
+                $id_empleados = Set::extract('/Empleado/id', $ids);
+
+                $hoy = date("d-m-Y");
+                $ids = $this->Contrato->find('all', array(
+                    'conditions' => array(
+                        'OR' => array(
+                            'FECHA_FIN > ' => $hoy,
+                            'FECHA_FIN' => NULL,
+                        ),
+                        'AND' => array(
+                            'MODALIDAD' => $modalidad,
+                            'empleado_id' => $id_empleados,
+                        )
+                    ),
+                    'contain' => array(
+                        'Empleado' => array(
+                            'fields' => array(
+                                'id')
+                        )
+                    )
+                        ));
+
+                $id_empleados = Set::extract('/Empleado/id', $ids);
+
+                $this->paginate = array(
+                    'Empleado' => array(
+                        'limit' => 20,
+                        'conditions' => array(
+                            'Empleado.id' => $id_empleados,
+                        ),
+                        'contain' => array(
+                            'Contrato' => array(
+                                'order' => array('Contrato.FECHA_INI' => 'asc'),
+                                'Cargo', 'Departamento'
+                            )
+                        )
+                    )
+                );
+                
                 $data = $this->Empleado->find('all', array(
                     'conditions' => array(
                         'Empleado.id' => $id_empleados,
@@ -95,6 +97,65 @@ class ReportesController extends AppController {
                 return;
             }
         }
+    }
+
+    function pantalla_listado($grupo = null, $modalidad = null) {
+        if ($grupo == "Todos") {
+            $grupo = array("Empleado", "Obrero");
+        }
+
+        $ids = $this->Empleado->Grupo->find('all', array(
+            'conditions' => array(
+                'NOMBRE' => $grupo
+            ),
+            'contain' => array(
+                'Empleado' => array(
+                    'fields' => array(
+                        'id'
+                    )
+                )
+            )
+                ));
+        $id_empleados = Set::extract('/Empleado/id', $ids);
+
+        $hoy = date("d-m-Y");
+        $ids = $this->Contrato->find('all', array(
+            'conditions' => array(
+                'OR' => array(
+                    'FECHA_FIN > ' => $hoy,
+                    'FECHA_FIN' => NULL,
+                ),
+                'AND' => array(
+                    'MODALIDAD' => $modalidad,
+                    'empleado_id' => $id_empleados,
+                )
+            ),
+            'contain' => array(
+                'Empleado' => array(
+                    'fields' => array(
+                        'id')
+                )
+            )
+                ));
+
+        $id_empleados = Set::extract('/Empleado/id', $ids);
+
+        $this->paginate = array(
+            'Empleado' => array(
+                'limit' => 20,
+                'conditions' => array(
+                    'Empleado.id' => $id_empleados,
+                ),
+                'contain' => array(
+                    'Contrato' => array(
+                        'order' => array('Contrato.FECHA_INI' => 'asc'),
+                        'Cargo', 'Departamento'
+                    )
+                )
+            )
+        );
+        $data = $this->paginate('Empleado');
+        $this->set('empleados', $data);
     }
 
 }
