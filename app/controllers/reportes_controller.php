7ba9b10111dcd80a9babe6eee161ecd75f690932
Michel Escobar <michelejcs@hotmail.com>
2012-06-28 19:28:47 -0430

    Eliminando un par de cosas

--- app/controllers/reportes_controller.php
+++ app/controllers/reportes_controller.php
@@ -17,44 +17,7 @@ class ReportesController extends AppController {
             $this->render('archivo_reporte','nominaExcel');
         }
         $this->set(compact('cargos','departamentos','asignaciones','deducciones'));
-    }
-    
-    function listado_persona(){
-        $filtro = array();
-        if (!empty($this->data)) {
-            if ($this->data['Fopcion'] == 1) {
-                $filtro = array('Empleado.CEDULA LIKE' => $this->data['valor']);
-            }
-            if ($this->data['Fopcion'] == 2) {
-                $filtro = array('Empleado.NOMBRE LIKE' => "%" . $this->data['valor'] . "%");
-            }
-            if ($this->data['Fopcion'] == 3) {
-                $filtro = array('Empleado.APELLIDO LIKE' => "%" . $this->data['valor'] . "%");
-            }
-        }
-        $this->paginate = array(
-            'limit' => 20,
-            'contain' => array(
-                'Grupo',
-                'Contrato' => array(
-                    'Cargo', 'Departamento',
-                    'order' => array(
-                        'Contrato.FECHA_INI' => 'desc'),
-                )
-                ));
-
-        $data = $this->paginate('Empleado', $filtro);
-        $this->set('empleados', $data);
-    }
-    
-    function reporte_persona($id=null){
-        $informacion=$this->Empleado->find('first',array(
-            'conditions'=>array(
-                'id'=>$id
-            )
-        ));
-    }
-
+    }     
 }
 
 ?>
